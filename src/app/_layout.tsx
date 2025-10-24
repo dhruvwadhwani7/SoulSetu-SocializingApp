ff5b94a62f59d43eae115754cfafb284acddb49a
PatelJetun <jetunpatel54@gmail.com>
2025-10-24 12:08:05 +0530

    Bottom Navigation Tab

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,10 @@
 import { Stack } from "expo-router";
-import '../../global.css';
+import "../../global.css";
 
 export default function Layout() {
-  return <Stack></Stack>;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(app)" />
+    </Stack>
+  );
 }
